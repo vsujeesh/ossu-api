52687af85787fa85d3964efc5a3eb773244db069
Oscar Gonzalez <oscarg610@gmail.com>
2015-10-17 17:40:01 -0400

    implement session

--- helpers/server.js
+++ helpers/server.js
@@ -5,6 +5,7 @@ require('dotenv').load();
 
 // load deps
 let express = require('express');
+let session = require('express-session');
 
 // load database
 let db = require('../helpers/database');
@@ -28,6 +29,10 @@ db['database'].once('connected', startServer);
 module.exports = app;
 
 function startServer () {
+  // use session storage
+  app.use(session({
+    secret: process.env.SESSION_SECRET || '0ThG9zitEectQpq7ThV2'
+  }));
   // set the api routes
   app.use('/api', Api(app));
   // mount auth routes
