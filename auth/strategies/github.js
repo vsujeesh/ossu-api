ca221b13312c576a681aa3f790cfbe2343507e8b
Oscar Gonzalez <oscarg610@gmail.com>
2015-10-18 00:56:28 -0400

    get authorization token from github

--- auth/strategies/github.js
+++ auth/strategies/github.js
@@ -3,6 +3,7 @@
 let express = require('express');
 let Router = express.Router();
 let AuthHelper = require('../../helpers/auth');
+let request = require('request');
 
 Router.use(AuthHelper.cors);
 
@@ -10,13 +11,28 @@ Router.post('/', (req, res) => {
   if (!req.body.code) {
     res.sendStatus(400).end();
   } else {
-    // TODO: get the token
-    res.sendStatus(200).end();
+    request({
+      method: 'POST',
+      uri: 'https://github.com/login/oauth/access_token',
+      body: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: req.body.code
+      },
+      json: true
+    }, (err, r, body) => {
+      if (err) {
+        return res.status(500).send((err) ? err.dsecription : 'Unknown Error.');
+      }
+
+      if (body.access_token) {
+        res.status(200).send(body);
+      }
+    });
   }
 });
 
 /** Export strategy information*/
-
 exports.name = 'Github';
 exports.endpoint = 'github';
 exports.router = Router;
